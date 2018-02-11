ef16522e44a67b9177008ccdce973a00468176ef
Scott Rees <reescott@gmail.com>
2018-02-11 15:34:32 -0800

    Move Pin # and Interval Delay into environment variables.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const Gpio = require("onoff").Gpio;
 const postEvent = require("./postEvent");
 
-const doorSwitch = new Gpio(22, "in");
+if (!process.env.DOOR_PIN || !process.env.INTERVAL_DELAY) {
+  throw new Error(
+    "Please configure .env file with DOOR_PIN and INTERVAL_DELAY"
+  );
+}
+
+const doorSwitch = new Gpio(process.env.DOOR_PIN, "in");
 let currentState = 0; // Defaults to closed
 
 setInterval(() => {
   doorSwitch.read(function(err, newState) {
-
     if (currentState === newState) return;
 
     const value2 = newState === 0 ? "closed" : "open";
@@ -14,8 +19,8 @@ setInterval(() => {
       value1: "Garage Door is ",
       value2
     };
-    process.stdOut.write(`Garage door is ${value2}`); 
+    process.stdOut.write(`Garage door is ${value2}`);
     currentState = newState;
     postEvent(eventData);
   });
-}, 2000);
+}, process.env.INTERVAL_DELAY);
